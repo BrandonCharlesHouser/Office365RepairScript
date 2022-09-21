fde2cfbac1e09af426fcb6575bb38ff8a55d0d08
BrandonCharlesHouser <114038470+BrandonCharlesHouser@users.noreply.github.com>
2022-09-21 09:35:11 -0500

    Update OfficeRepair.ps1
    Cleaned up and added more comments
    Added better error handling
    Replaced if else statements with switch statements for better readability.

--- OfficeRepair.ps1
+++ OfficeRepair.ps1
@@ -1,13 +1,21 @@
 param (
     # If set to false ALL Office 365 apps will be force closed and repair will occurr in the background. User will not have any progress notifications until Teams begins reinstalling and the script completes.
     [switch] $Background,
+    # If set brings up help text
     [switch] $help,
     [switch] $h
 )
 
+##############################################################################
+#                                 Help Text                                  #
+##############################################################################
+
 function DisplayHelp {
     # Get-Help OfficeRepair -Detailed
     write-host @"
+"Office Repair 1.0 Help Guide"
+"Script by Brandon Houser: https://github.com/BrandonCharlesHouser/"
+
 NAME
 OfficeRepair
 
@@ -57,26 +65,62 @@ Force closes all Office 365 applications and performs a background repair.
 "@
 }
 
+##############################################################################
+#                           Function Definitions                             #
+##############################################################################
+
+# Check if command exists
+Function Test-CommandExists ($command) {
+    $oldPreference = $ErrorActionPreference
+    $ErrorActionPreference = "stop"
+    try {if(Get-Command $command){$True}}
+    Catch {$false}
+    Finally {$ErrorActionPreference=$oldPreference}
+}
+
 function Write-HostAndMsgBox($MessageText) {
-    Start-Process -FilePath "CMD.exe" -ArgumentList '/c', 'msg', '*', $MessageText
+    if ($msgCommandPresent) {
+        $process = Start-Process -FilePath "CMD.exe" -ArgumentList '/c', 'msg', '*', $MessageText
+    }
+    Write-Host ($MessageText)
+}
+
+function ThrowAndMsgBox($MessageText) {
+    if ($msgCommandPresent) {
+        $process = Start-Process -FilePath "CMD.exe" -ArgumentList '/c', 'msg', '*', $MessageText
+    }
     Throw ($MessageText)
 }
-if ($help.IsPresent -or $h.IsPresent) {
-    write-host "Office Repair 1.0 Help Guide"
-    write-host "Script by Brandon Houser: https://github.com/BrandonCharlesHouser/"
 
-    DisplayHelp
+##############################################################################
+#                            Parameter Handling                              #
+##############################################################################
+
+# Checks if command line utility "msg" exists (Not present on Windows 10 Home)
+$msgCommandPresent = Test-CommandExists "msg"
+
+switch ($help.IsPresent -or $h.IsPresent)
+{
+    $True
+        {
+            DisplayHelp
+            exit
+        }
+    $False
+        {
+            # Check if running in elevated session
+            $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+            $AmIAdmin = $($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+
+            if (-not $AmIAdmin) {
+                $AdminErrorText = "You must run script as Administrator."
+                ThrowAndMsgBox $AdminErrorText
+            }
+        }
 }
-else {
-
-    # Check if running in elevated session
-    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-    $AmIAdmin = $($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-
-    if (-not $AmIAdmin) {
-        $AdminErrorText = "You must run script as Administrator."
-        Write-HostAndMsgBox $AdminErrorText
-    }
+##############################################################################
+#                           Initialize Variables                             #
+##############################################################################
 
     #Set Variables
     $UserDownloads = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
@@ -87,31 +131,59 @@ else {
     $MinimumRunTime = New-TimeSpan -Minutes 5
 
     # Finds whether user has x86 or x64 version of Office365 installed
-    $OfficeBitVersion = ($(Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration" | Select-Object Platform).platform)
-
-    # Writes to terminal whether you have 32/64 bit Office and throws an exception/aborts script if an unexpected result appears
-    if ("x86" -eq $OfficeBitVersion) {
-        Write-Host "32-Bit Office365 installation found."
+    switch (Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration")
+    {
+        $True
+            {
+                $OfficeBitVersion = ($(Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration" | Select-Object Platform).platform)
+            }
+        $False
+            {
+                ThrowAndMsgBox "Microsoft Office 365 not installed."
+            }
     }
-    elseif ("x64" -eq $OfficeBitVersion) {
-        Write-Host "64-Bit Office365 installation found."
-        }
-    else {Throw ("Office365 version not found. Manually run repair.")}
+    # Writes to terminal whether you have 32/64 bit Office and throws an exception/aborts script if an unexpected result appears
+    switch ($OfficeBitVersion)
+    {
+        "x86"
+            {
+                Write-Host "32-Bit Office365 installation found."
+            }
+        "x64"
+            {
+                Write-Host "64-Bit Office365 installation found."
+            }
+        default
+            {
+                ThrowAndMsgBox "Office365 version not found. Manually run repair."
+            }
+    }
 
     # Builds the Platform parameter for OfficeClickToRun.exe
     $OfficePlatform = $("platform=$OfficeBitVersion")
 
     # Checks if Background Flag is set then builds the DisplayLevel and forceappshutdown parameters for OfficeClickToRun.exe
-    if ($Background.IsPresent) {
-        $OfficeDisplayLevel = @("DisplayLevel=False", "forceappshutdown=True")
-    } Else {
-        $OfficeDisplayLevel = @("DisplayLevel=True")
+    switch ($Background.IsPresent)
+    {
+        $True
+            {
+                $OfficeDisplayLevel = @("DisplayLevel=False", "forceappshutdown=True")
+            }
+        $False
+            {
+            $OfficeDisplayLevel = @("DisplayLevel=True")
+            }
     }
+##############################################################################
+#                                Main Script                                 #
+##############################################################################
+#                               Office Repair                                #
+##############################################################################
 
     # Builds the argument list for OfficeClickToRun.exe
     $OfficeRepairArgList = @("scenario=Repair", "$OfficePlatform", "culture=en-us", "RepairType=FullRepair") + $OfficeDisplayLevel
 
-    # Writes command to be run to the Terminal
+    # Writes command to be sused in Start-Process
     Write-Host ('Start-Process "C:\Program Files\Microsoft Office 15\ClientX64\OfficeClickToRun.exe" -ArgumentList -NoNewWindow -Wait -PassThru ' + $OfficeRepairArgList)
     Set-Location -Path "C:\Program Files\Microsoft Office 15\ClientX64\"
 
@@ -120,28 +192,33 @@ else {
     # Subtracts Process Start Time and Exit Time to get the run time as a TimeSpan Object  
     $processRunTime = ($process.StartTime - $process.ExitTime)
 
-
-
+##############################################################################
+#                              Teams Reinstall                               #
+##############################################################################
 
     # If Repair Program errors out or quits unexpectedly it will return an error code other than 0
     # Checks if Error Code is not 0 AND if Process Run Time equals Minimum Run Time
-    if ($($process.ExitCode -eq 0) -and $($processRunTime -ge $MinimumRunTime)) {
-        #Teams Download and Install - Repairs sometimes cause Teams to disappear sometimes even a day later, reinstalling prevents this$TeamsInstallerWebLocation = "https://go.microsoft.com/fwlink/p/?LinkID=2187327&clcid=0x409&culture=en-us&country=US"
-        Invoke-WebRequest -UseBasicParsing -Uri $TeamsInstallerWebLocation -outfile $TeamsInstallerLocation
-        Start-Process -FilePath $TeamsInstallerLocation -NoNewWindow -Wait -PassThru
+    switch ($($process.ExitCode -eq 0) -and $($processRunTime -ge $MinimumRunTime))
+    {
+        $True
+            {
+                #Teams Download and Install - Repairs sometimes cause Teams to disappear sometimes even a day later, reinstalling prevents this$TeamsInstallerWebLocation = "https://go.microsoft.com/fwlink/p/?LinkID=2187327&clcid=0x409&culture=en-us&country=US"
+                Invoke-WebRequest -UseBasicParsing -Uri $TeamsInstallerWebLocation -outfile $TeamsInstallerLocation
+                Start-Process -FilePath $TeamsInstallerLocation -NoNewWindow -Wait -PassThru
 
-        # Writes Message to terminal and creates a Message Box with the same text
-        $ExitText = "Office repaired"
-        Write-HostAndMsgBox $ExitText
+                # Writes Message to terminal and creates a Message Box with the same text
+                $ExitText = "Office repaired"
+                Write-HostAndMsgBox $ExitText
+            }
+        $False
+            {
+                # Writes Message to terminal and creates a Message Box with the same text
+                $ExitText = "Office Not Repaired"
+                Write-Host $ExitText
+                if (-not ($process.ExitCode) -eq 0) {
+                    $ExitCode = "Error Code: $($process.ExitCode)"
+                    Write-HostAndMsgBox $ExitCode
+                }
+                Write-HostAndMsgBox $ExitText
+            }
     }
-    Else {
-        # Writes Message to terminal and creates a Message Box with the same text
-        $ExitText = "Office Not Repaired"
-        Write-Host $ExitText
-        if (-not ($process.ExitCode) -eq 0) {
-            $ExitCode = "Error Code: $($process.ExitCode)"
-            Write-HostAndMsgBox $ExitCode
-        }
-        Write-HostAndMsgBox $ExitText
-    }
-}
